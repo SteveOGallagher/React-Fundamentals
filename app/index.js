c2979af2b07b2fec9e421cbfa19f4c31c4006726
Steve O'Gallagher <steve.ogallagher@cohaesus.co.uk>
2016-11-02 08:49:22 +0000

    Incude basic routing into app.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,71 @@
+var USER_DATA = {
+  name: "Steve O'Gallagher",
+  username: "AddedSteve",
+  image: "https://pbs.twimg.com/profile_images/600016970780651520/MMUQ1gu7.jpg"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
+    return <img src={this.props.imageUrl} style={ {height: 100, width: 100} } />
+  }
+});
+
+var Link = React.createClass({
+  changeURL: function(){
+    window.location.replace(this.props.href)
+  },
+
+  render: function(){
     return (
-      <div> Hello World </div>
+      <span style={{ color: 'blue', cursor: 'pointer'}}
+       onClick={this.changeURL}>
+        {this.props.children}
+      </span>
     )
   }
 });
 
+var ProfileLink = React.createClass({ 
+  render: function() {
+    return  (
+      <div>
+        <Link href={'https://www.twitter.com/' + this.props.username}>
+          {this.props.username}
+        </Link>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return  (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass ({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl = {this.props.user.image} />
+        <ProfileName name = {this.props.user.name} />
+        <ProfileLink username = {this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+var routes = require('./config/routes');
+
 ReactDOM.render(
-  <HelloWorld />,
+  // <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
